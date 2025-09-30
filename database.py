b91bd585698f9303aa5ed7b5277c292065bc5427
KarimStaff <karimawudulai@gmail.com>
2025-09-30 17:49:26 +0000

    cc

--- database.py
+++ database.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DEBUG DATABASE_URL =", os.getenv("DATABASE_URL"))
+
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Add it in Railway Variables.")
 
